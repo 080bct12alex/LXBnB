b1cbbd26dd45d494c98cee962d37e9043ad68d97
Alex Shrestha <080bct012.alex@pcampus.edu.np>
2025-06-03 08:12:24 +0545

    first commit

---
+++ frontend/src/config/hotel-options-config.ts
@@ -0,0 +1,20 @@
+export const hotelTypes = [
+  "Budget",
+  "Luxury",
+  "Business",
+  "Family",
+  "Romantic",
+  "Hiking Resort",
+  "Beach Resort",
+  
+];
+
+export const hotelFacilities = [
+  "Free WiFi",
+  "Parking",
+  "Airport Shuttle",
+  "Family Rooms",
+  "Non-Smoking Rooms",
+  "Outdoor Pool",
+  "Fitness Center",
+];
